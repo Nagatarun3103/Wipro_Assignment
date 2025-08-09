431e9195dd367369003fe6a5db939901069d314e
Nagatarun3103 <100620482+Nagatarun3103@users.noreply.github.com>
2025-08-09 10:40:14 +0530

    Delete Wipro_Assignment directory

--- Wipro_Assignment/src/main/java/house/Hall.java
+++
@@ -1,9 +0,0 @@
-package house;
-
-import static java.lang.System.out; 
-
-public class Hall {
-    public void display() {
-        out.println("This is the first room while entering the house");
-    }
-}
