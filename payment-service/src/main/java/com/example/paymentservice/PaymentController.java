366e099bf89309c595a80193f951ce73341b7f6a
Nagatarun3103 <100620482+Nagatarun3103@users.noreply.github.com>
2025-08-11 09:55:31 +0530

    zipkin

---
+++ payment-service/src/main/java/com/example/paymentservice/PaymentController.java
@@ -0,0 +1,12 @@
+package com.example.paymentservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PaymentController {
+    @GetMapping("/payment")
+    public String makePayment() {
+        return "Payment successful";
+    }
+}
