c497403628f4ce51fb73ca95ffd5202ddc3ef778
Nagatarun3103 <100620482+Nagatarun3103@users.noreply.github.com>
2025-08-13 08:37:36 +0530

    Delete src directory

--- src/main/java/com/javadeveloperzone/model/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.javadeveloperzone.model;
--- src/main/java/com/javadeveloperzone/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.javadeveloperzone.repository;
-
-import com.javadeveloperzone.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
