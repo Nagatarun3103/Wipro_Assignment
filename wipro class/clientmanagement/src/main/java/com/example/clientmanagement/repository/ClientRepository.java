a098adc194d5c729a12631d298b4912daee162fa
Nagatarun3103 <100620482+Nagatarun3103@users.noreply.github.com>
2025-08-13 08:38:57 +0530

    Delete wipro class/clientmanagement directory

--- wipro class/clientmanagement/src/main/java/com/example/clientmanagement/repository/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.clientmanagement.repository;
-
-import com.example.clientmanagement.entity.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client, Long> {
-}
